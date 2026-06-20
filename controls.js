58d7286e24b930c5c05570906200dcea91ac6065
David Jarvis <ddjarvis@up.edu.ph>
2026-06-20 16:43:21 +0800

    .

--- controls.js
+++ controls.js
@@ -33,31 +33,28 @@ document.addEventListener("keydown", (event) => {
 
 let startX, startY;
 const threshold = 50; // minimum swipe distance
-function armTouchListeners() {
-    document.addEventListener('touchstart', (e) => {
-    startX = e.touches[0].clientX;
-    startY = e.touches[0].clientY;
+document.addEventListener('touchstart', (e) => {
+startX = e.touches[0].clientX;
+startY = e.touches[0].clientY;
+e.preventDefault(); // stops page scrolling
+},{ passive: false });
+
+document.addEventListener('touchend', (e) => {
     e.preventDefault(); // stops page scrolling
-    },{ passive: false });
+const endX = e.changedTouches[0].clientX;
+const endY = e.changedTouches[0].clientY;
 
-    document.addEventListener('touchend', (e) => {
-        e.preventDefault(); // stops page scrolling
-    const endX = e.changedTouches[0].clientX;
-    const endY = e.changedTouches[0].clientY;
+const diffX = startX - endX;
+const diffY = startY - endY;
 
-    const diffX = startX - endX;
-    const diffY = startY - endY;
-
-    if (Math.abs(diffX) > Math.abs(diffY) && Math.abs(diffX) > threshold) {
-        if (diffX > threshold) action('left');
-        else if (diffX < -threshold) action('right');
-    } else if (Math.abs(diffY) > Math.abs(diffX) && Math.abs(diffY) > threshold) {
-        if (diffY > threshold) action('up');
-        else if (diffY < -threshold) action('down');
-    }
-    },{ passive: false });
+if (Math.abs(diffX) > Math.abs(diffY) && Math.abs(diffX) > threshold) {
+    if (diffX > threshold) action('left');
+    else if (diffX < -threshold) action('right');
+} else if (Math.abs(diffY) > Math.abs(diffX) && Math.abs(diffY) > threshold) {
+    if (diffY > threshold) action('up');
+    else if (diffY < -threshold) action('down');
 }
-
+},{ passive: false });
 
 
 function action(direction) {
